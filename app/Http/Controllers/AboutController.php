737755a405b164c5c51ebea27014836430aa7a22
usernameAnda <emailAnda>
2021-09-12 13:05:33 +0700

    Menambahkan 3 controller dan 3 function

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return "Nama: Irva Putri Finisha dan NIM: 2031710103";
+    }
+}
